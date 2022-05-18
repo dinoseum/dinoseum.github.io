e534bf99d73c121319edc059e631c08e146f4771
Davide Ciacco <davide.ciacco@neosperience.com>
2022-05-18 11:50:38 +0200

    deployable react app

---
+++ README.md
@@ -0,0 +1,2 @@
+# DINOSEUM
+### website
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <strong>
+        <small>DINOSEUM</small>
+      </strong>
+    </div>
+  );
+}
+
+export default App;
